f1bcd487f693c1a9db3c9e4bbce48811d3ebc61b
Christian Boyle <mail@christianboyle.com>
2023-07-04 09:42:27 -0500

    cleanup

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # Importing necessary packages, files and services
 import os 
 import openai
-
-
 import streamlit as st 
+
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
@@ -21,7 +20,7 @@ st.title('🔋👨‍⚖️ ParetoPal')
 prompt = st.text_input('What topic would you like to research: ') 
 
 # Prompt templates
-title_template = PromptTemplate(
+body_template = PromptTemplate(
     input_variables = ['topic'], 
     template='Please forget all prior prompts.  You are a university professor at a top university. You have become an expert in the Pareto principle (80/20 rule). Please identify the 20-percent of {topic} that will yield 80-percent of the best  results. Use your academic resources to  provide a well identified and focused learning program to master this subject.'
 )
@@ -30,15 +29,15 @@ title_template = PromptTemplate(
 wiki = WikipediaAPIWrapper()
 
 # Memory 
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+body_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 
 # Llms
 llm = ChatOpenAI(model_name="gpt-4", temperature=0.9, openai_api_key=openai_api_key) 
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+body_chain = LLMChain(llm=llm, prompt=body_template, verbose=True, output_key='body', memory=body_memory)
 
 # Chaining the components and displaying outputs
 if prompt: 
-    title = title_chain.run(prompt)
+    body = body_chain.run(prompt)
     wiki_research = wiki.run(prompt) 
 
-    st.write(title) 
+    st.write(body) 
